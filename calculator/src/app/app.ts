2fedb4351e97d54fe59bfc2b5c87caada08e2c07
Daniel Delchev <daniel.delchev@yahoo.com>
2026-05-08 18:40:29 +0300

    initial commit

---
+++ calculator/src/app/app.ts
@@ -0,0 +1,12 @@
+import { Component, signal } from '@angular/core';
+import { Header } from "./header/header";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.html',
+  styleUrl: './app.css',
+  imports: [Header]
+})
+export class App {
+  protected readonly title = signal('calculator');
+}
